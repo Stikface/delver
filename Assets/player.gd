20e93f05ea0a2e8a68e9ddec43024d525c92332f
DoktorBeta <doktorbeta1556@gmail.com>
2025-07-31 19:23:24 -0700

    player controller
    gravity is commented out because we were falling through the floor. add gravity next time!
    
    also, a floor texture.
    
    we turned into a capsule temporarily to try to fix the falling-through-floor. that didn't work, so we can turn into a cylinder next time.
    
    inputmap got changes too. add analog stick inputs at home

---
+++ Assets/player.gd
@@ -0,0 +1,66 @@
+extends CharacterBody3D
+#stolen from youtube channel Gwizz "Godot 4 First Person Controller" https://www.youtube.com/watch?v=fAVetlIROXM
+
+const MOVE_SPEED = 5.0
+
+#get project's universal gravity setting
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var look_dir: Vector2
+var look_sensitivity = 50
+@onready var camera = $Camera3D
+
+var mouse_capture = false
+
+#this runs for each physics tick.
+#functions with _ at the start are supposed to be private and not accessed by other scripts
+func _physics_process(delta):
+	_rotate_camera(delta)
+	_movement_handler(delta)
+
+#this gets called every time any kind of input happens
+func _input(event: InputEvent):
+	#input events are defined by the InputMap but mouse inputs are Their Own Thing
+	#event.relative is the amount the input variable changed since the last frame
+	if event is InputEventMouseMotion: look_dir = event.relative * 0.01
+	
+	#toggle mouse capture
+	if Input.is_action_pressed("Pause"):
+		mouse_capture = !mouse_capture #flips the boolean to the opposite of what it already was. ! = "NOT"
+		if mouse_capture:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _rotate_camera(delta: float, sensitivity_modifier: float = 1.0):
+	var input_dir = Input.get_vector("LookLeft","LookRight","LookDown","LookUp")
+	look_dir += input_dir
+	#rotate the whole player node left and right
+	rotation.y -= look_dir.x * look_sensitivity * delta
+	#rotate just the camera up and down. clamp() so you can't break your neck looking up and down
+	camera.rotation.x = clamp(camera.rotation.x - look_dir.y * look_sensitivity * sensitivity_modifier * delta, -1.5, 1.5) #clamp bounds are in radians
+	look_dir = Vector2.ZERO
+
+func _movement_handler(delta: float):
+	#falling down
+	#if not is_on_floor():
+	#	velocity.y -= gravity * delta
+
+	#we don't have jumping so im not adding a jump function
+
+	# get input directions for movement. this gives intensity 0.0 to 1.0 if using analog sticks
+	var input_dir = Input.get_vector("Left","Right","Up","Down")
+	# create a Vector3 using input directions. normalized() stops the "diagonal is faster" thing that happens in DOOM
+	var movement_dir = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if movement_dir:
+		#if there's input, set velocity to SPEED
+		velocity.x = movement_dir.x * MOVE_SPEED
+		velocity.z = movement_dir.z * MOVE_SPEED
+	else:
+		#interpolate towards 0 velocity when not taking input
+		velocity.x = move_toward(velocity.x, 0, MOVE_SPEED)
+		velocity.z = move_toward(velocity.z, 0, MOVE_SPEED)
+
+	#builtin function that handles movement and slides when it crashes into things
+	move_and_slide()
